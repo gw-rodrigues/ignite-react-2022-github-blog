1bec09274a22bdead3ad16fb1139bd1904dacc83
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-14 21:36:41 +0100

    feat: add page element

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Header } from './components/Header'
+import { BlogList } from './pages/blog-list'
+
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div className="bg-base-background w-full min-h-screen pb-20">
+      <Header />
+      <main className="max-w-[864px] mx-auto mt-[-5.4rem] text-base-text">
+        <BlogList />
+      </main>
     </div>
   )
 }
