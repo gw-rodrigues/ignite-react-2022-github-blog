b1ad3cac898b527a82f40543aabec8ec3be43251
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-14 17:52:25 +0100

    build: install tailwindcss and config

---
+++ tailwind.config.cjs
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'Nunito, sans-serif',
+      },
+
+      backgroundImage: {
+        header: 'url(/header-bg.svg)',
+      },
+
+      colors: {
+        blue: '#129e57',
+        base: {
+          title: '#E7EDF4',
+          subtitle: '#C4D4E3',
+          text: '#AFC2D4',
+          span: '#7B96B2',
+          label: '#3A536B',
+          border: '#1C2F41',
+          post: '#112131',
+          profile: '#0B1B2B',
+          background: '#071422',
+          input: '#040F1A',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
