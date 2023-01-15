b139357d654cf371132ad6361a157b2aaa896035
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-15 19:16:45 +0100

    feat: moving input self component

--- src/pages/blog-list/components/search-articles.tsx
+++ src/pages/blog-list/components/search-articles.tsx
@@ -1,16 +1,13 @@
+import { Input } from '../../../components/Input'
+
 export function SearchArticles() {
   return (
     <div className="flex flex-col gap-3">
       <div className="w-full flex justify-between">
-        <h2>Publicações</h2>
-        <div>6 publicações</div>
+        <h2 className="text-lg text-base-subtitle">Publicações</h2>
+        <span className="text-sm text-base-span">6 publicações</span>
       </div>
-      <input
-        type="text"
-        name="search"
-        className="w-full px-3 py-4 bg-base-input border border-base-border rounded-md text-base-text placeholder:text-base-label"
-        placeholder="Buscar conteúdo"
-      />
+      <Input name="search" type="text" placeholder="Buscar conteúdo" />
     </div>
   )
 }
