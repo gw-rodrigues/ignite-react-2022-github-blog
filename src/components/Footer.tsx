40170bc41337c8a33ef94d4b2b9356f151b1856e
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-15 19:18:36 +0100

    feat: create footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+export function Footer() {
+  return (
+    <div className="text-base-label text-xs text-center m-8">
+      Developed by{' '}
+      <a
+        href="https://github.com/gw-rodrigues"
+        className="border-b border-transparent hover:border-base-label transition-colors"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Gleydson W. Rodrigues
+      </a>{' '}
+      - 2023
+    </div>
+  )
+}
