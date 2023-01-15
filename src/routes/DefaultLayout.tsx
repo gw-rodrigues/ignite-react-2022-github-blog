525c1ecb2ecd718e5991261e3cf3947f3b171bc8
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-15 19:45:45 +0100

    feat: create default layout theme

---
+++ src/routes/DefaultLayout.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom'
+import { Footer } from '../components/Footer'
+import { Header } from '../components/Header'
+
+export function DefaultLayout() {
+  return (
+    <div className="bg-base-background w-full min-h-screen pb-20">
+      <Header />
+      <main className="max-w-[864px] mx-auto mt-[-5.4rem] text-base-text px-10 lg:px-0">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  )
+}
