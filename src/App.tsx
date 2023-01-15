ce5a1e9aae026f1a6194cce48d995299448d5a6f
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-15 19:19:27 +0100

    feat: add blog article page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import { Footer } from './components/Footer'
 import { Header } from './components/Header'
-import { BlogList } from './pages/blog-list'
+import { BlogArticle } from './pages/blog-article'
 
 function App() {
   return (
     <div className="bg-base-background w-full min-h-screen pb-20">
       <Header />
-      <main className="max-w-[864px] mx-auto mt-[-5.4rem] text-base-text">
-        <BlogList />
+      <main className="max-w-[864px] mx-auto mt-[-5.4rem] text-base-text px-10 lg:px-0">
+        <BlogArticle />
       </main>
+      <Footer />
     </div>
   )
 }
