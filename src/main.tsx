67ecee7b5ed7b602e48210a1d72f4059055fd0cf
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-15 19:45:20 +0100

    feat: add routes provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { RoutesProvider } from './routes/RoutesProvider'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RoutesProvider />
   </React.StrictMode>,
 )
