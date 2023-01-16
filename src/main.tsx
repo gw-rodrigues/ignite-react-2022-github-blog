63f0b01e5839232cc71a6eef50592dca59361d18
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-16 11:38:05 +0100

    feat: add context provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BlogProvider } from './contexts/BlogContext'
 import { RoutesProvider } from './routes/RoutesProvider'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RoutesProvider />
+    <BlogProvider>
+      <RoutesProvider />
+    </BlogProvider>
   </React.StrictMode>,
 )
