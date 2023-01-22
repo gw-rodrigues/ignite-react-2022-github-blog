7bdd25c015a24fed5020fadff6146ed961d16aa1
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-22 20:03:59 +0100

    fix: routes

--- src/routes/RoutesProvider.tsx
+++ src/routes/RoutesProvider.tsx
@@ -1,25 +1,28 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { BlogArticle } from '../pages/blog-article'
-import { BlogList } from '../pages/blog-list'
+
 import { DefaultLayout } from './DefaultLayout'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <DefaultLayout />,
-    children: [
-      {
-        path: '/',
-        element: <BlogList />,
-      },
-      {
-        path: '/post/:id',
-        element: <BlogArticle />,
-      },
-    ],
-  },
-])
+import { BlogArticle } from '../pages/blog-article'
+import { BlogList } from '../pages/blog-list'
 
 export function RoutesProvider() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <DefaultLayout />,
+      children: [
+        {
+          index: true,
+          path: '/',
+          element: <BlogList />,
+        },
+        {
+          path: '/post/:id',
+          element: <BlogArticle />,
+        },
+      ],
+    },
+  ])
+
   return <RouterProvider router={router} />
 }
