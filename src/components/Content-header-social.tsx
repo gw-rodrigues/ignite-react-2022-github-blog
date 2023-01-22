2fbf80e02d3df04868c47e8d7041e1eeeef0ff6b
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-22 20:02:36 +0100

    fix: convert type dates to string

--- src/components/Content-header-social.tsx
+++ src/components/Content-header-social.tsx
@@ -11,7 +11,7 @@ interface IContentHeaderSocialProps {
   github?: string
   company?: string
   followers?: number
-  createdAt?: Date
+  createdAt?: string
   comments?: number
 }
 
@@ -27,37 +27,37 @@ export function ContentHeaderSocial({
   }
 
   return (
-    <div className="flex gap-6 mt-2 text-base-subtitle text-sm flex-wrap justify-center md:justify-start">
+    <div className="flex flex-wrap justify-center gap-6 mt-2 text-sm text-base-subtitle md:justify-start">
       {github && (
-        <div className="flex gap-2 items-center">
+        <div className="flex items-center gap-2">
           <GithubLogo size={18} weight="fill" className="fill-base-label" />
           <span>{github}</span>
         </div>
       )}
       {company && (
-        <div className="flex gap-2 items-center">
+        <div className="flex items-center gap-2">
           <Buildings size={18} weight="fill" className="fill-base-label" />
           <span>{company}</span>
         </div>
       )}
       {followers && (
-        <div className="flex gap-2 items-center">
+        <div className="flex items-center gap-2">
           <Users size={18} weight="fill" className="fill-base-label" />
           <span>{followers} seguidores</span>
         </div>
       )}
       {createdAt && (
-        <div className="flex gap-2 items-center">
+        <div className="flex items-center gap-2">
           <Calendar size={18} weight="fill" className="fill-base-label" />
-          <span>{dateFormatter(createdAt)}</span>
+          <span>{dateFormatter(new Date(createdAt))}</span>
         </div>
       )}
-      {comments && (
-        <div className="flex gap-2 items-center">
+      {comments !== undefined && comments >= 0 ? (
+        <div className="flex items-center gap-2">
           <ChatCircle size={18} weight="fill" className="fill-base-label" />
           <span>{comments} cometários</span>
         </div>
-      )}
+      ) : null}
     </div>
   )
 }
